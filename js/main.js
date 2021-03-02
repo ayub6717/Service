223f364c889624bc3f7c4eb9a9adfc34aadc1e6d
ayub6717 <ayub15-6717@diu.edu.bd>
2021-03-02 13:08:11 +0600

    Category

--- js/main.js
+++ js/main.js
@@ -61,5 +61,75 @@ function responseMessage(msg) {
     $('.success-box div.text-message').html("<span>" + msg + "</span>");
 }
 //*****************star ratting end****************** */
-
+$(document).ready(function() {
+  
+    // Initialise Selectric Dropdown and Slick Carousel
+    $('#categories').selectric();
+    
+    $('.responsive').slick({
+        // dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                // centerMode: true,
+  
+            }
+        }, {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+                infinite: true,
+  
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+  
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        }, {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        }]
+    });
+    
+    var currentIndex = 0;
+    $('#categories').on('change', function() {
+      currentIndex = $(this).prop('selectedIndex');
+      var currentSlide = $('.responsive').slick('slickCurrentSlide');
+      $('.responsive').slick('slickGoTo', parseInt(currentIndex));
+    });
+    
+    
+    $('.category .show-more-link').on('click', function() {
+      $(this).next().css('display', 'block');
+    });
+    
+    
+    $('.category .close').on('click', function() {
+      $(this).parent().css('display', 'none');
+    });
+  
+  
+  });
 
