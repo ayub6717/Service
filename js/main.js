6ae9e0f6d028dbff8fd210d8272e73deb168f6c6
ayub6717 <ayub15-6717@diu.edu.bd>
2021-03-02 20:02:13 +0600

    Add Scroll top button

--- js/main.js
+++ js/main.js
@@ -381,3 +381,19 @@ function hideChat(hide) {
 }
 
 // <!-- **********Live Chat end**************** -->
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
